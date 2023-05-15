08d36d42f79d08e3f913f452ec944b72e92fb92a
Ayaanz20 <125305567+Ayaanz20@users.noreply.github.com>
2023-05-15 18:55:13 +0100

    Add files via upload

---
+++ AI Baby Monitoring Web App/main.js
@@ -0,0 +1,58 @@
+song="";
+status='';
+objects=[];
+function preload() { song = loadSound("alarm.mp3"); }
+function setup(){
+canvas=createCanvas(400,400);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(400,400);
+video.hide();
+objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML='Status-Detecting Objects';
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded!");
+    status=true;
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }console.log(results);
+    objects=results;
+
+}
+function draw(){
+image(video,0,0,400,400);
+if(status != ''){
+    r=random(255);
+    g=random(255);
+    b=random(255);
+    objectDetector.detect(video, gotResult)
+    for(
+        var i=0; i<objects.length; i++){
+document.getElementById('status').innerHTML='status=objects detected';
+            fill(255,0,0);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255,0,0);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label == "person"){
+                document.getElementById('number_of_objects').innerHTML='baby found';
+                console.log('stop');
+                song.stop();
+            }
+            else{
+                document.getElementById('number_of_objects').innerHTML='baby not found';
+                console.log('play');
+                song.play();
+            }
+        }
+        if(objects.length == 0){
+            document.getElementById('number_of_objects').innerHTML='baby not found';
+            console.log('play');
+            song.play();
+        }
+}}
